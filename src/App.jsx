6337eec12d4e38d108976ec1aeaf3fda0bc7a235
Javoh <117713938+mugambijavan@users.noreply.github.com>
2025-01-15 00:32:33 +0300

    Add initial project setup with Vite, React, Tailwind CSS, and essential components

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+
+const socialLinks = [
+  { href: "https://discord.com", icon: <FaDiscord /> },
+  { href: "https://twitter.com", icon: <FaTwitter /> },
+  { href: "https://youtube.com", icon: <FaYoutube /> },
+  { href: "https://medium.com", icon: <FaMedium /> },
+];
+
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-[#ffffff] py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm font-light md:text-left">
+          ©So Awesome 2025. All rights reserved 🚀
+        </p>
+
+        <div className="flex justify-center gap-4  md:justify-start">
+          {socialLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+
+        <a
+          href="https://bhakitah.co.ke/"
+          target="_blank"
+          className="text-center text-sm font-light hover:underline md:text-right"
+        >
+          BhakitahTech
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
